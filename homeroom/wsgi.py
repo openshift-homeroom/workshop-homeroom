7fcc5c44eff5a0df2a4a973b23b5a1f34a08208d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-04-04 15:39:54 +1100

    Filter out hidden list entries.

--- homeroom/wsgi.py
+++ homeroom/wsgi.py
@@ -8,14 +8,21 @@ app = Flask(__name__)
 
 workshops = []
 
+def filter_out_hidden(workshops):
+    for workshop in workshops:
+        if workshop.get('visibility', 'visible') != 'hidden':
+            yield workshop
+
 if os.path.exists('/opt/app-root/configs/workshops.json'):
     with open('/opt/app-root/configs/workshops.json') as fp:
-        workshops = json.load(fp)
+        workshops = list(filter_out_hidden(json.load(fp)))
 
 if not workshops:
     for i in range(10):
         workshops.append(dict(title='Workshop #%s'%i,
             description='This is workshop #%s.'%i, url='http://www.example.com'))
+
+print('WORKSHOPS', workshops)
 
 @app.route('/')
 def index():
